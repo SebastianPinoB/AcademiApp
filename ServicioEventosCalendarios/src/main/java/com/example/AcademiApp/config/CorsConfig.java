88a20eb51cabe8c7e272cc922f46ef3fdd2e561a
hoerests23 <ba.moyav@duocuc.cl>
2026-06-28 05:24:57 -0400

    Conexion back con front de eventos,ajustes servicio mensajeria y puerto cors para front

--- ServicioEventosCalendarios/src/main/java/com/example/AcademiApp/config/CorsConfig.java
+++ ServicioEventosCalendarios/src/main/java/com/example/AcademiApp/config/CorsConfig.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
+
 }
---
+++ ServicioEventosCalendarios/src/main/java/com/example/AcademiApp/model/dto/AsignaturaExternoDTO.java
@@ -0,0 +1,10 @@
+package com.example.AcademiApp.model.dto;
+
+import lombok.Data;
+
+@Data
+public class AsignaturaExternoDTO {
+    private int asigId;
+    private String asigNombre;
+    private String asigDesc;
+}
